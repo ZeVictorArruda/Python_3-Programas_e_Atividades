384e6218386e24f9fa3a24acf6ef8f41eb22f30d
ZeVictorArruda <jvictoraaraujo911@gmail.com>
2020-05-09 13:13:54 -0300

    update peso_ideal

--- peso_ideal.py
+++ peso_ideal.py
@@ -1,20 +1,37 @@
-#cálculo de peso ideal através de IMC
-altura = float(input('Insira sua altura: '))
-peso = float(input('Insira seu peso: '))
-imc = peso / (altura ** 2)
-if 18.5 < imc <= 24.9:
-    print('Você está dentro do peso ideal.')
-else:
-    peso_minimo = 18.6 * (altura ** 2)
-    peso_maximo = 24.9 * (altura ** 2)
-    if imc <= 18.5:
-        print('Você está abaixo do peso ideal.')
+#cálculo de peso ideal através de IMC utilizando listas e dicionários para armazenar os dados
+pessoa = {}
+lista = []
+
+while True:
+    pessoa['nome'] = str(input('Nome: ')).strip().capitalize()
+    pessoa['altura'] = float(input('Altura: '))
+    pessoa['peso'] = float(input('Peso: '))
+    imc = round(pessoa['peso'] / (pessoa['altura'] ** 2), 2)
+    pessoa['imc'] = imc
+    if 18.5 >= imc:
+        pessoa['situação'] = 'Abaixo do peso ideal'
+    elif 18.5 < imc <= 24.9:
+        pessoa['situação'] = 'Peso ideal'
     elif 25 <= imc <= 29.9:
-        print('Você está acima do peso.')
+        pessoa['situação'] = 'Acima do peso'
     elif 30 <= imc <= 34.9:
-        print('Você está com obesidade grau 1')
+        pessoa['situação'] = 'Obesidade grau 1'
     elif 35 <= imc <= 39.9:
-        print('Você está com obesidade grau 2')
+        pessoa['situação'] = 'Obesidade grau 2'
     else:
-        print('Você está com obesidde grau 3')
-    print(f'Seu peso ideal se encontra entre {round(peso_minimo, 2)}kg e {round(peso_maximo, 2)}kg')
+        pessoa['situação'] = 'Obesidade grau 3'
+    lista.append(pessoa.copy())
+    pessoa.clear()
+    resp = str(input('Continuar cadastro? (S/N): ')).lower().strip()
+    while resp not in 'sn':
+        resp = str(input('Comando inválido. Deseja continuar o cadastro? (Digite S para sim ou N para não) '))
+    if resp == 'n':
+        break
+
+print('-' * 30)
+print(f'{"|No":<3} {"|NOME":<15} {"|ALTURA":<15} {"|PESO":<15} {"|IMC":<15} {"|SITUAÇÃO":<20}')
+for i, c in enumerate(lista):
+    print(f'|{i+1:<3}', end='')
+    for k, v in c.items():
+        print(f'|{v:<15}', end='')
+    print()
