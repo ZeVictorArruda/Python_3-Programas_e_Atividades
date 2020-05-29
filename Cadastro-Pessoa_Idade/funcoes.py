4ddd5b90a06a5ff84941238bfcf321826d883231
ZeVictorArruda <jvictoraaraujo911@gmail.com>
2020-05-28 23:51:46 -0300

    Funções adicionadas

--- Cadastro-Pessoa_Idade/funcoes.py
+++ Cadastro-Pessoa_Idade/funcoes.py
@@ -25,3 +25,38 @@ def menu(lista):
     print('-' * 40)
     opc = lerint('Digite sua opção: ')
     return opc
+
+
+def arqexiste(nome):
+    try:
+        a = open(nome, 'rt')
+        a.close
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+
+def criararquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close
+    except:
+        print('Erro ao criar arquiv.')
+    else:
+        print(f'Arquivo {nome} criado com sucesso!')
+
+
+def lerarquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler arquivo')
+    else:
+        for linha in a:
+            cabecalho('PESSOAS CADASTRADAS')
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
